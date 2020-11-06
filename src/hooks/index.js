8acb8df7b2dc0352c68394d44185cd30361fd328
josue-castro <josue.castro@upr.edu>
2020-11-06 15:48:52 -0400

    index file to export custom hooks

---
+++ src/hooks/index.js
@@ -0,0 +1,4 @@
+import useTeamRosters from './useTeamRosters';
+import useGameStatus from './useGameStatus';
+import useGameActions from './useGameActions';
+export { useTeamRosters, useGameStatus, useGameActions };
