ee5d9f81ebbdee020ac30ea68632066afa6d706e
josue-castro <josue.castro@upr.edu>
2020-11-06 15:48:26 -0400

    Custom hook to fetch both uprm and opponent team rosters

---
+++ src/hooks/useTeamRosters.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import { firebase } from '_firebase';
+
+/**
+ * Fetch team rosters for both UPRM and the opponent given an event
+ * @param {number} eventId ID of the event
+ * @param {boolean} gameOver Game status, ended or ongoing
+ */
+const useTeamRosters = (eventId, gameOver = false) => {
+  const [uprmRoster, setUprmRoster] = useState({});
+  const [opponentRoster, setOpponentRoster] = useState({});
+  useEffect(() => {
+    const uprmRosterRef = database.ref(`v1/${eventId}/uprm-roster`);
+    const opponentRosterRef = database.ref(`v1/${eventId}/opponent-roster`);
+    // If the game has ended only fetch the rosters once since there will be no changes
+    if (gameOver) {
+      uprmRosterRef.once('value', rosterSnapshot =>
+        setUprmRoster(rosterSnapshot.val())
+      );
+      opponentRosterRef.once('value', rosterSnapshot =>
+        setOpponentRoster(rosterSnapshot.val())
+      );
+    } else {
+      // If the game is not over listen to roster changes
+      uprmRosterRef.on('value', rosterSnapshot =>
+        setUprmRoster(rosterSnapshot.val())
+      );
+      opponentRosterRef.once('value', rosterSnapshot =>
+        setOpponentRoster(rosterSnapshot.val())
+      );
+    }
+
+    return () => {
+      uprmRosterRef.off();
+      opponentRosterRef.off();
+    };
+  }, [eventId, gameOver]);
+  return { uprm: uprmRoster, opponent: opponentRoster };
+};
+
+export default useTeamRosters;
