f690086b738e3f5d941bb823d5eacb3bef3a7968
josue-castro <josue.castro@upr.edu>
2020-11-16 10:10:10 -0400

    Card to display results information in the Results View

---
+++ src/components/ResultCard.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ResultCard = ({ sportName, uprmName, opponentName, location }) => {
+  return (
+    <View style={styles.eventCard}>
+      <View style={styles.cardHeader}>
+        <Text style={styles.headerText}>{sportName}</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  eventCard: {
+    flex: 1,
+    height: 80,
+    margin: 10,
+    borderRadius: 5,
+    backgroundColor: '#fff',
+    alignItems: 'stretch',
+  },
+  cardHeader: {
+    backgroundColor: '#1B7744',
+    height: 25,
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  headerText: {
+    fontWeight: 'bold',
+    color: 'white',
+    fontSize: 12,
+  },
+  cardText: {
+    paddingHorizontal: 20,
+    textAlign: 'left',
+  },
+});
+
+export default ResultCard;
