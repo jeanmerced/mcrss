cdc7c498d80986529ebb635ba5210bc409f5ba16
josue-castro <josue.castro@upr.edu>
2020-11-26 18:46:41 -0400

    Add elevation styling

---
+++ src/styles/elevations.js
@@ -0,0 +1,18 @@
+export const depth2 = {
+  shadowOffset: { width: 0, height: 1 },
+  shadowOpacity: 0.2,
+  shadowRadius: 1.41,
+  elevation: 2,
+};
+export const depth3 = {
+  shadowOffset: { width: 0, height: 1 },
+  shadowOpacity: 0.22,
+  shadowRadius: 2.22,
+  elevation: 3,
+};
+export const depth4 = {
+  shadowOffset: { width: 0, height: 2 },
+  shadowOpacity: 0.23,
+  shadowRadius: 2.62,
+  elevation: 4,
+};
